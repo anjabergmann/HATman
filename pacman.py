f5951115d7902430061845c1dbf899cfa7636045
gitTARDIS <anja.bergmann@hotmail.com>
2016-04-15 22:21:43 +0200

    test with four crossNodes (corners)

--- pacman.py
+++ pacman.py
@@ -87,30 +87,58 @@ class GameScene(Scene):
 		self.pressedKey = None;
 		self.direction = key.RIGHT;
 
+		self.crossNodes = self.labLayer.crossNodes;
+
 	#TODO:
 	# Überprüfen, ob Pacman sich an einem Node befindet,
 	# an dem er in die gewünschte Richtung abbiegen kann
 	# Wenn möglich: Direction auf die gewünschte Direction ändern
 	# Sonst in die aktuelle Direction weiterfahren bis anstehen
 	def setDirection(self):
-		print("TODO GameScene.setDirection()");
 		self.pressedKey = self.pacmanLayer.pressedKey;
-		self.direction = self.pressedKey;
-		self.pacmanLayer.direction = self.direction;
+		if self.pressedKey != self.direction:
+			if self.pressedKey == (key.RIGHT and self.direction == key.LEFT) or (self.pressedKey == key.LEFT and self.direction == key.RIGHT) or (self.pressedKey == key.DOWN and self.direction == key.UP) or (self.pressedKey == key.UP and self.direction == key.DOWN):
+				self.direction = self.pressedKey;
+				self.pacmanLayer.direction = self.direction;
+			else:
+				for cn in self.crossNodes:
+					if self.pacmanLayer.pacmanRect.x == cn.x and self.pacmanLayer.pacmanRect.y == cn.y:
+						if self.pressedKey == key.RIGHT and cn.nodeRight != None:
+							self.direction = self.pressedKey;
+							self.pacmanLayer.direction = self.direction;
+						elif self.pressedKey == key.LEFT and cn.nodeLeft != None:
+							self.direction = self.pressedKey;
+							self.pacmanLayer.direction = self.direction;
+						elif self.pressedKey == key.UP and cn.nodeUp != None:
+							self.direction = self.pressedKey;
+							self.pacmanLayer.direction = self.direction;
+						elif self.pressedKey == key.DOWN and cn.nodeDown != None:
+							self.direction = self.pressedKey;
+							self.pacmanLayer.direction = self.direction;
 
-	#Check if pacman reaches border or deadlock
+
+
+	#Check if pacman reaches border 
+	#or [TODO] deadlock
+	#	if pacman reaches Node where neither "direction" nor "pressedKey"
+	#	is a possible option
+	# --> stand still
 	def checkBorders(self):
 		if self.pacmanLayer.direction == key.RIGHT:
-			if self.pacmanLayer.pacmanRect.right >= self.labLayer.labRect.right:
+			#if self.pacmanLayer.pacmanRect.right >= self.labLayer.labRect.right:
+			if self.pacmanLayer.pacmanRect.x >= 600:
 				self.pacmanLayer.direction = None;
 		elif self.pacmanLayer.direction == key.LEFT:
-			if self.pacmanLayer.pacmanRect.left <= self.labLayer.labRect.left:
+			#if self.pacmanLayer.pacmanRect.left <= self.labLayer.labRect.left:
+			if self.pacmanLayer.pacmanRect.x <= 40:
 				self.pacmanLayer.direction = None;
 		elif self.pacmanLayer.direction == key.UP:
-			if self.pacmanLayer.pacmanRect.top >= self.labLayer.labRect.top:
+			#if self.pacmanLayer.pacmanRect.top >= self.labLayer.labRect.top:
+			if self.pacmanLayer.pacmanRect.y >= 420:
 				self.pacmanLayer.direction = None;
 		elif self.pacmanLayer.direction == key.DOWN:
-			if self.pacmanLayer.pacmanRect.bottom <= self.labLayer.labRect.bottom:
+			#if self.pacmanLayer.pacmanRect.bottom <= self.labLayer.labRect.bottom:
+			if self.pacmanLayer.pacmanRect.y <= 40:
 				self.pacmanLayer.direction = None;
 
 	def update(self, director):
@@ -129,7 +157,7 @@ class LabLayer(Layer):
 		super(LabLayer, self).__init__();
 
 		self.labRect = Rect(20, 40, 600, 400);
-		self.potentialNodes = [];
+		self.potentialNodes = []; #Zweidimensionale Arrays wären vielleicht besser ...
 		self.crossNodes = [];
 
 		# write every "point" of the Labyrinth into a list
@@ -139,7 +167,22 @@ class LabLayer(Layer):
 				self.potentialNodes.append(tempNode);
 
 		#TODO Choose some Nodes that are crossNodes (random?)
-		self.crossNodes = self.potentialNodes;
+		#self.crossNodes = self.potentialNodes;
+
+		#For testing purposes only four nodes in the corners are crossNodes
+		self.crossNodes.append(self.potentialNodes[0]); #links unten
+		self.crossNodes.append(self.potentialNodes[28]); #rechts unten
+		self.crossNodes.append(self.potentialNodes[551]); #links oben
+		self.crossNodes.append(self.potentialNodes[579]); #rechts oben
+
+		self.crossNodes[0].nodeUp = self.crossNodes[2];
+		self.crossNodes[0].nodeRight = self.crossNodes[1];
+		self.crossNodes[1].nodeUp = self.crossNodes[3];
+		self.crossNodes[1].nodeLeft = self.crossNodes[0];
+		self.crossNodes[2].nodeRight = self.crossNodes[3];
+		self.crossNodes[2].nodeDown = self.crossNodes[0];
+		self.crossNodes[3].nodeLeft = self.crossNodes[2];
+		self.crossNodes[3].nodeDown = self.crossNodes[1];
 
 
 		print("INFO labRect.top", self.labRect.top);
@@ -170,7 +213,7 @@ class PacmanLayer(Layer):
 		self.pacmans.append(self.pacman1);
 		self.pacmans.append(self.pacman2);
 
-		self.pacmanRect = Rect(20, 40, self.pacman1.width * 0.1, self.pacman1.height * 0.1);
+		self.pacmanRect = Rect(40, 40, self.pacman1.width * 0.1, self.pacman1.height * 0.1);
 
 		for pacman in self.pacmans:
 			self.add(pacman);
@@ -182,6 +225,9 @@ class PacmanLayer(Layer):
 		print("INFO pacman.bottom ", self.pacmanRect.bottom);
 		print("INFO pacman.left ", self.pacmanRect.left);
 		print("INFO pacman.right ", self.pacmanRect.right);
+
+		print("INFO pacmanRect.x ", self.pacmanRect.x);
+		print("INFO pacmanRect.y ", self.pacmanRect.y);
 
 
 		#Animate pacman
