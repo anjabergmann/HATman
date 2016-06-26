28bc71b380d205fbd6e42f251aaf7762c1731736
gitTARDIS <anja.bergmann@hotmail.com>
2016-06-26 16:38:24 +0200

    parseargs

--- parse.py
+++ parse.py
@@ -10,18 +10,18 @@ for i in range (0, 10):
 print(user)
 
 
-parser = argparse.ArgumentParser(description='Process some names')
-parser.add_argument('--user', action='store_true', default=user, help='user flag')
-parser.add_argument('string', action='store')
+parser = argparse.ArgumentParser(description='Parse_args_testing')
+
+
+parser.add_argument('-u', '--user', type=str, required=False, default=user, help='Your username.')
+parser.add_argument('-c', '--character', type=str, required=False, default="pacman", choices=["pac", "r", "o", "b", "p"], help='The character you want to play: pac(man), r(ed ghost), o(range ghost), b(lue ghost), p(ink ghost)')
+
 
 args = parser.parse_args()
 
 
-
-if args.user:
-	print("~ Your user name is: " + str(args.string))
-else:
-	print("~ You did not enter a user name after '--user'")
+print("Your user name is: " + args.user)
+print("You are playing: " + args.character)
 
 
 
